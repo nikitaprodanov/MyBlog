74f9c53c1cfb179f37476094828c9b14d80baf6a
Nikita <uchihanaruto9371@gmail.com>
2020-06-11 17:40:59 +0300

    Edit user credentials

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for, jsonify, send_from_directory, flash, session
 
+from functools import wraps
+
 from database import DB
 from user import User
 
 app = Flask(__name__)
 app.secret_key = "blog and posts"
+
+def require_login(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not session.get('logged_in'):
+            return redirect('/login')
+        return func(*args, **kwargs)
+    return wrapper
 
 @app.route('/')
 def main_page():
@@ -14,7 +24,7 @@ def main_page():
     return redirect('/posts')
 
 @app.route('/posts_logged_in')
-# @require_login
+@require_login
 def posts_logged_in():
     # posts = Post.all()
     # images = {}
@@ -23,7 +33,7 @@ def posts_logged_in():
     #         directory = os.listdir(post.file_path) 
     #         file_path = post.file_path
     #         images.update({file_path : directory[0]})
-    return render_template('posts_logged_in.html') #''', posts = posts, username = User.find_by_id(session['USERNAME']), images = images''')
+    return render_template('posts_logged_in.html', username = User.find_by_id(session['USERNAME'])) #''', posts = posts, images = images''')
 
 @app.route('/posts')
 def list_posts():
@@ -92,6 +102,84 @@ def login():
         # logging.info('%s with id: %s successfully logged in', User.find_by_id(session['USERNAME']), session['USERNAME'])
 
         return redirect('/')
+
+@app.route('/user_info')
+@require_login
+def user_info():
+    username = User.find_by_id(session['USERNAME'])
+    return render_template('user_info.html', user = User.find_by_username(username))
+
+@app.route('/edit_user_username', methods=['POST'])
+def edit_user_username():
+    if request.method == 'POST':
+        username = User.find_by_id(session['USERNAME'])
+        user = User.find_by_username(username)
+        edit_username = request.form['username']
+        if User.find_by_username(edit_username):
+            flash('This username is already registered!')
+            return redirect('/user_info')
+        edit_oldpassword = request.form['oldpassword']
+        if not user or not user.verify_password(edit_oldpassword):
+            flash('Incorrect password!')
+            return redirect('/user_info')
+        user.username = edit_username
+        
+        # logging.info('%s with id: %s changed his username to %s', User.find_by_id(session['USERNAME']), session['USERNAME'], edit_username)
+        
+        User.save_username(user)
+
+        return redirect('/user_info')
+
+@app.route('/edit_user_email', methods=['POST'])
+def edit_user_email():
+    if request.method == 'POST':
+        username = User.find_by_id(session['USERNAME'])
+        user = User.find_by_username(username)
+        edit_email = request.form['email']
+        if User.find_by_email(edit_email):
+            flash('This email is already registered!')
+            return redirect('/user_info')
+        edit_oldpassword = request.form['oldpassword']
+        if not user or not user.verify_password(edit_oldpassword):
+            flash('Incorrect password!')
+            return redirect('/user_info')
+        user.email = edit_email
+        User.save_email(user)
+        
+        # logging.info('%s with id: %s changed his email to %s', User.find_by_id(session['USERNAME']), session['USERNAME'], edit_email)
+        
+        return redirect('/user_info')
+
+@app.route('/edit_user_password', methods=['POST'])
+def edit_user_password():
+    if request.method == 'POST':
+        username = User.find_by_id(session['USERNAME'])
+        user = User.find_by_username(username)
+        edit_oldpassword = request.form['oldpassword']
+        if not user or not user.verify_password(edit_oldpassword):
+            flash('Incorrect password!')
+            return redirect('/user_info')
+        edit_password = request.form['password']
+        edit_confirmpassword = request.form['confirmpassword']
+        if not edit_password == edit_confirmpassword:
+            flash('Incorrect password confirmation!')
+            return redirect('/user_info')
+        user.password = User.hash_password(request.form['password'])
+        User.save_password(user)
+        
+        # logging.info('%s with id: %s changed his password', User.find_by_id(session['USERNAME']), session['USERNAME'])
+        
+        return redirect('/user_info')
+
+@app.route('/log_out', methods=['GET','POST'])
+@require_login
+def log_out():
+    # logging.info('%s with id: %s logged out', User.find_by_id(session['USERNAME']), session['USERNAME'])
+    
+    session['USERNAME'] = None
+    session['logged_in'] = False
+
+    return redirect('/')
 
 # TODO ADD POSTS ARTICLES AND COMMENTS FUNCTIONALITY
 
--- user.py
+++ user.py
@@ -1,7 +1,7 @@
+
 import hashlib
 
 from database import DB
-# from post import Post
 
 class User:
     def __init__(self, id, username, password, email):
@@ -17,6 +17,43 @@ class User:
                 INSERT INTO users (username, password, email)
                 VALUES (?, ?, ?)''', values)
             return self
+    
+    @staticmethod
+    def find_by_email(email):
+        if not email:
+            return None
+        with DB() as db:
+            row = db.execute(
+                'SELECT * FROM users WHERE email = ?',
+                (email,)
+            ).fetchone()
+            if row:
+                return User(*row)
+
+
+    @staticmethod
+    def find_by_username(username):
+        if not username:
+            return None
+        with DB() as db:
+            row = db.execute(
+                'SELECT * FROM users WHERE username = ?',
+                (username,)
+            ).fetchone()
+            if row:
+                return User(*row)
+
+    @staticmethod
+    def find_by_id(id):
+        if not id:
+            return None
+        with DB() as db:
+            row = db.execute(
+                'SELECT username FROM users WHERE id = ?',
+                (id,)
+            ).fetchone()
+            if row:
+                return row[0]
 
     @staticmethod
     def hash_password(password):
@@ -24,3 +61,18 @@ class User:
 
     def verify_password(self, password):
         return self.password == hashlib.md5(password.encode('utf-8')).hexdigest()
+
+    def save_username(self):
+        with DB() as db:
+            values = (self.username, self.id)
+            db.execute('''UPDATE users SET username = ? WHERE id = ?''', values)
+    
+    def save_email(self):
+        with DB() as db:
+            values = (self.email, self.id)
+            db.execute('''UPDATE users SET email = ? WHERE id = ?''', values)
+
+    def save_password(self):
+        with DB() as db:
+            values = (self.password, self.id)
+            db.execute('''UPDATE users SET password = ? WHERE id = ?''', values)
