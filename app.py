06cde02a53a6e6d674e1e24c66897e8fb19ec1ef
Nikita <uchihanaruto9371@gmail.com>
2020-06-11 15:08:58 +0300

    App routes for logged in users and logged out

--- app.py
+++ app.py
@@ -6,7 +6,36 @@ app.secret_key = "blog and posts"
 
 @app.route('/')
 def main_page():
-    return render_template('hello.html')
+    if session.get('logged_in'):
+        return redirect('/posts_logged_in')
+    return redirect('/posts')
+
+@app.route('/posts_logged_in')
+@require_login
+def posts_logged_in():
+    # posts = Post.all()
+    # images = {}
+    # if posts:
+    #     for post in posts:
+    #         directory = os.listdir(post.file_path) 
+    #         file_path = post.file_path
+    #         images.update({file_path : directory[0]})
+    return render_template('posts_logged_in.html' ''', posts = posts, username = User.find_by_id(session['USERNAME']), images = images''')
+
+@app.route('/posts')
+def list_posts():
+    if session.get('logged_in'):
+        return redirect('/posts_logged_in')
+    # posts = Post.all()
+    # images = {}
+    # if posts:
+    #     for post in posts:
+    #         directory = os.listdir(post.file_path) 
+    #         file_path = post.file_path
+    #         images.update({file_path : directory[0]})
+    return render_template('posts.html' ''', posts = posts, images = images''')
+
+# TODO ADD POSTS ARTICLES AND COMMENTS FUNCTIONALITY
 
 
 if __name__ == '__main__':
