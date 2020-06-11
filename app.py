ed7816ec2c0a37cb3cc0b4bae3a157c66f6bc2c3
Nikita <uchihanaruto9371@gmail.com>
2020-06-11 14:48:35 +0300

    App config

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template, request, redirect, url_for, jsonify, send_from_directory, flash, session
+
+app = Flask(__name__)
+app.secret_key = "blog and posts"
+
+@app.route('/')
+def main_page():
+    return 'Hello world!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
